93eb0f8eca9b0b9ffdd31837c487b3811c804cfd
Joan M Acom <marthajoan10@gmail.com>
2026-06-29 12:43:29 +0300

    Added JS checker

--- check.py
+++ check.py
@@ -1,7 +1,33 @@
 import requests
 import ast
+import esprima
 
-def analyze_file(path):
+def analyze_js_file(path):
+    with open(path, "r", encoding="utf-8") as file:
+        source_code=file.read()
+
+    tree= esprima.parseModule(source_code)
+    imports = []
+
+#esprima returns a body list of nodes  
+    for node in tree.body:
+        # Check for 'import ... from "package"'
+        if node.type == 'ImportDeclaration':
+            # The source is the string part (e.g., 'express')
+            pkg = node.source.value
+            imports.append(pkg)
+            
+    return set(imports)
+
+def is_package_on_pypi(package_name):
+    url = f"https://pypi.org/pypi/{package_name}/json"
+    try:
+        response = requests.get(url, timeout=5)
+        return response.status_code == 200# 200 means it exists
+    except requests.RequestException:
+        return False#assumes fals if it doesn't exist
+   
+def analyze_py_file(path):
     with open(path, "r", encoding="utf-8") as file:
         tree = ast.parse(file.read())#parse file into an AST
 
@@ -17,10 +43,11 @@ def analyze_file(path):
                 imports.append(node.module)
     return set(imports)
 
-def is_package_on_pypi(package_name):
-    url = f"https://pypi.org/pypi/{package_name}/json"
+ 
+def is_package_on_npm(package_name):
+    url = f"https://registry.npmjs.org/{package_name}"
     try:
         response = requests.get(url, timeout=5)
         return response.status_code == 200# 200 means it exists
     except requests.RequestException:
-        return False#assumes fals if it doesn't exist
+        return False
--- scanner.py
+++ scanner.py
@@ -1,9 +1,17 @@
-from check import analyze_file, is_package_on_pypi
+from check import analyze_js_file, analyze_py_file, is_package_on_pypi
 
-found_packages = analyze_file("test_script.py")
+def get_imports(file_path):
+    if file_path.endswith(".py"):
+        return analyze_py_file(file_path)
+    elif file_path.endswith(".js"):
+        return analyze_js_file(file_path)
+    else:
+        raise ValueError("Unsupported file type")
+    
+found_packages = get_imports("test_script.py")
 print(f"Packages found: {found_packages}")
 for pkg in found_packages:
     if is_package_on_pypi(pkg):
-        print(f"[SAFE] {pkg} exists on PyPI")
+        print(f"[SAFE] {pkg} exists")
     else:
-        print(f"[SAFE] {pkg} was not foun on PyPI-possible hallucination")
+        print(f"[SAFE] {pkg} was not found")
