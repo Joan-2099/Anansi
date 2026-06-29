4d24a7b83c90fb47be489c67a10544da1007bea3
Joan M Acom <marthajoan10@gmail.com>
2026-06-29 12:52:14 +0300

    Combined func to scan files

--- scanner.py
+++ scanner.py
@@ -1,17 +1,23 @@
-from check import analyze_js_file, analyze_py_file, is_package_on_pypi
+from check import analyze_js_file, analyze_py_file,is_package_on_npm, is_package_on_pypi
 
 def get_imports(file_path):
     if file_path.endswith(".py"):
-        return analyze_py_file(file_path)
+        imports = analyze_py_file(file_path)
+        lang = "Python"
+        check_func = is_package_on_pypi
     elif file_path.endswith(".js"):
-        return analyze_js_file(file_path)
+        imports = analyze_js_file(file_path)
+        lang = "JavaScript"
+        check_func = is_package_on_npm
+        return 
     else:
-        raise ValueError("Unsupported file type")
-    
-found_packages = get_imports("test_script.py")
-print(f"Packages found: {found_packages}")
-for pkg in found_packages:
-    if is_package_on_pypi(pkg):
-        print(f"[SAFE] {pkg} exists")
-    else:
-        print(f"[SAFE] {pkg} was not found")
+        print("Unsupported file")
+        return
+
+    for pkg in imports:
+        if check_func(pkg):
+            print(f"[✅ SAFE] {pkg} exists in the {lang} registry")
+        else:
+            print(f"[❌ Alert] {pkg} was not found in the {lang} registry")
+
+get_imports("test_script.py")
